fa0f9117c406fa2385b7064519a52ff23caa6677
KalyanovAnton <kalyanov.anton@gmail.com>
2026-06-25 13:39:15 +0200

    Add Terraform modules for S3, VPC, and ECR

---
+++ lesson-5/modules/s3-backend/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  description = "Унікальне ім'я для S3 бакета"
+}
+
+variable "table_name" {
+  type        = string
+  description = "Ім'я таблиці DynamoDB"
+}

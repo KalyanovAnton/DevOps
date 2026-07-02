ab2fdff37773b61e1b870bacaa9b7f56b8a2d028
KalyanovAnton <kalyanov.anton@gmail.com>
2026-07-02 13:14:14 +0200

    add eks

--- lesson-5/backend.tf
+++ lesson-5/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket       = "lesson-5-kalianov-bucket"
-    key          = "lesson-5/terraform.tfstate"
-    region       = "us-west-2"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket       = "lesson-5-kalianov-bucket"
+#     key          = "lesson-5/terraform.tfstate"
+#     region       = "us-west-2"
+#     use_lockfile = true
+#     encrypt      = true
+#   }
+# }
